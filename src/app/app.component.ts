793b99ca7a84113df5b5917ea93cb8cb724efc75
Kodavoor <734456@cognizant.com>
2018-11-27 12:04:16 +0530

    Created Angular Project for Full Stack Web App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template: '<h1>Hello Everyone!!</h1>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular-Front-End';
+  message = 'Good Night'
 }
